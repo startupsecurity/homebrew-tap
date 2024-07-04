e2679d81c608a566d40491eb7b606de6493654b7
ryanlabouve <imryanlabouve@gmail.com>
2024-07-03 23:28:44 -0500

    Bumps minos to 0.0.29

--- minos-codex.rb
+++ minos-codex.rb
@@ -3,12 +3,12 @@ class MinosCodex < Formula
     homepage "github.com/startupsecurity/minos-codex"
     if Hardware::CPU.arm?
       url "https://github.com/startupsecurity/minos-codex/releases/download/v0.0.28/minos-codex-aarch64-apple-darwin-macos-latest.tar.gz"
-      sha256 "ad41c3ebc0ffaccce495e908e1256204470999b9591965682c2ed958fb69e1fe"
+      sha256 "0f44decfebca40011a323b0eadbaad4c3e8f7b81d261de7633b3c170d321d81f"
     else
       url "https://github.com/startupsecurity/minos-codex/releases/download/v0.0.28/minos-codex-x86_64-apple-darwin-macos-latest.tar.gz"
-      sha256 "de96fab0b30ee8249fdbd86f718d815c20030fc547a71e4cd92d20daf20bd0f1"
+      sha256 "4b65c41de50d8d357e4b3451faeb24fd11a41f28688f73905bdd17b111642db7"
     end
-    version "0.0.28"
+    version "0.0.29"
   
     def install
       bin.install "minos-codex"
